3730030cd179673b387166494ec0d5a766b8d4af
Doug Hitzel <DougHitzel@hotmail.com>
2024-08-26 14:32:32 -0400

    Handles browser refresh

--- UserManager/client/src/main.ts
+++ UserManager/client/src/main.ts
@@ -1,10 +1,10 @@
 /// <reference types="@angular/localize" />
 
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [ provideRouter(routes),
